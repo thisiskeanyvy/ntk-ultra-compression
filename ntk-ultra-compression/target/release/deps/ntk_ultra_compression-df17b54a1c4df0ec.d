102738022840ee9e82c0192ce388c9814c0fa6e0
thisiskeanyvy <keanyvy@keemail.me>
2025-02-04 00:38:00 +0100

    README update

--- ntk-ultra-compression/target/release/deps/ntk_ultra_compression-df17b54a1c4df0ec.d
+++
@@ -1,5 +0,0 @@
-/home/drwho/Documents/ntk-project/logiciel/ntk-ultra-compression/target/release/deps/ntk_ultra_compression-df17b54a1c4df0ec: src/main.rs
-
-/home/drwho/Documents/ntk-project/logiciel/ntk-ultra-compression/target/release/deps/ntk_ultra_compression-df17b54a1c4df0ec.d: src/main.rs
-
-src/main.rs:
--- ntk-ultra-compression/target/release/ntk-ultra-compression.d
+++
@@ -1,1 +0,0 @@
-/home/drwho/Documents/ntk-project/logiciel/ntk-ultra-compression/target/release/ntk-ultra-compression: /home/drwho/Documents/ntk-project/logiciel/ntk-ultra-compression/src/main.rs
