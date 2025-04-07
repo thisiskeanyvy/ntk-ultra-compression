89518c6b21c266d42db2110573ad1e7ffd6b2d21
thisiskeanyvy <keanyvy@keemail.me>
2025-04-08 00:31:12 +0200

    rust linking

--- gui_ntk_ultra_compression/src-tauri/src/main.rs
+++ gui_ntk_ultra_compression/src-tauri/src/main.rs
@@ -1,6 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[tauri::command]
+fn ma_fonction_rust() {
+    println!("vous avez vraiment cru qu'on allait coder pour avoir 14.08 au lieu de 20 au projet, fuck epita !!!")
+}
+
 fn main() {
     gui_ntk_ultra_compression_lib::run()
 }
